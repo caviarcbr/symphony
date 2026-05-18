abf6e792f409b1e4c66029d30f5092471cc2998a
Jacob Zawadzki <jtzawadzki22@gmail.com>
2026-05-18 12:17:31 -0500

    Fixed conflicting keybinds

--- config/hypr/media.lua
+++ config/hypr/media.lua
@@ -31,18 +31,12 @@ hl.bind("ALT + XF86MonBrightnessDown", hl.dsp.exec_cmd(OsdClient .. " --brightne
 hl.bind("SUPER + ALT + F10", hl.dsp.exec_cmd(OsdClient .. " --brightness +1"), { repeating = true, locked = true} )
 hl.bind("SUPER + ALT + F9", hl.dsp.exec_cmd(OsdClient .. " --brightness -1"), { repeating = true, locked = true} )
 
--- Requires playerctl
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd(OsdClient .. " --playerctl next"), {  locked = true} )
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd(OsdClient .. " --playerctl play-pause"), {  locked = true} )
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(OsdClient .. " --playerctl play-pause"), {  locked = true} )
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(OsdClient .. " --playerctl previous"), {  locked = true} )
+-- mpc
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd(OsdClient .. " mpc next"), {  locked = true} )
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd(OsdClient .. " mpd toggle"), {  locked = true} )
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(OsdClient .. " mpd toggle"), {  locked = true} )
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(OsdClient .. " mpd prev"), {  locked = true} )
 
 -- Switch audio output with Super + Mute
 hl.bind("SUPER + XF86AudioMute", hl.dsp.exec_cmd(ScrDir .. "/audio-switch"), {  locked = true} )
-
--- mpc
-hl.bind("SUPER + F9", hl.dsp.exec_cmd("mpc pause"))
-hl.bind("SUPER + F10", hl.dsp.exec_cmd("mpd prev"))
-hl.bind("SUPER + F11", hl.dsp.exec_cmd("mpc play"))
-hl.bind("SUPER + F12", hl.dsp.exec_cmd("mpc next"))
 
