a034966afd97f6573134c4d3cfe5c294f62f5942
Jacob Zawadzki <jtzawadzki22@gmail.com>
2026-05-20 01:36:59 -0500

    Updated waybar hook

--- install/themes/hooks/10-waybar.sh
+++ install/themes/hooks/10-waybar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # waybar - symlink colors and restart
 src="$CURRENT_LINK/waybar/colors.css"
+pid=$(pgrep -x waybar)
 [[ -f "$src" ]] || exit 0
 ln -sf "$src" "$HOME/.config/waybar/colors.css"
 
-pkill -x waybar 2>/dev/null
+kill -9 "$pid" 2>/dev/null
 sleep 0.3
 setsid uwsm-app -- waybar >/dev/null 2>&1 &
 exit 0
